f985e3fb8f0bd83526def1b751c2501898795eb0
Nick Sutterer <apotonick@gmail.org>
2008-07-28 00:58:44 +0200

    cleaned up Apotomo::ViewHelper and added tests.

--- apotomo/app/helpers/apotomo/view_helper.rb
+++ apotomo/app/helpers/apotomo/view_helper.rb
@@ -7,56 +7,82 @@ module Apotomo
     end
     
     
-    ### DISCUSS: find the tree via current_widget, 
-    ### pass the tree obj, or what?
     def current_tree
       Apotomo::StatefulWidget.current_widget.root
     end
     
     
-    ### TODO: deprecate.
-    ### DISCUSS: really?
-    #def link_to_event(title, source_id=false, way={}, html_options={})
+    # public methods ------------------------------------------------------------
+    
+    # Creates a link to the event controller and the targeted widget.
+    # The AJAX request triggered by clicking this link will result in an Apotomo event
+    # with the specified :source and :type, along with arbitrary parameters in way.
+    # Reserved options for way:
+    #   :source
+    #   :type
+    ### TODO: discuss the request cycle behaviour.
     def link_to_event(title, way={}, html_options={})
-      source_id = way[:source] || false
-      ### TODO: filter out :source from way!
-      addr = address_to_remote_widget(source_id, way)
+      source_id = way[:source]
+      addr      = address_to_remote_widget(source_id, way)
       
       link_to_remote(title, {:url => addr}, html_options)
     end
     
-    
-    # public methods ------------------------------------------------------------
-    
+    # Returns the address hash to the event controller and the targeted widget.
+    # Can be passed to #url_for.
     def address_to_remote_widget(widget_id=false ,way={}) 
       target = target_widget_for(widget_id)
-        ### TODO: utilize Apotomo::Addressing.
-      #way.merge({:action => :event_gateway, :controller => 'apotomo', :widget_id => target.name})
-      way.merge({:action => :event, :controller => 'apotomo', :source => target.name})
+      way.merge({:action => 'event', :controller => 'apotomo', :source => target.name})
+    end
+    
+    
+    # Creates a bookmarkable link to a widget. When clicked, the whole application
+    # state ("page") is reloaded without AJAX.
+    def static_link_to_widget(title, widget_id=false, way={}, html_options={})
+      target = target_widget_for(widget_id)
+      way.delete(:static) ### DISCUSS: clean up at central place.
+      
+      link_to(title, target.address(way), html_options)
     end
     
     
     # the standard way to get a link tag referencing a widget in the tree.
     ### DISCUSS: rename to #link_to_invoke ?
     def link_to_widget(title, widget_id=false, way={}, html_options={})
-      way[:type] = :invoke  ### FIXME: not necessary, since this is set in the apotomo_controller by default.
-      #link_to_event(title, widget_id, way, html_options)
+      if way[:static]
+        return static_link_to_widget(title, widget_id, way, html_options)
+      end
+      
+      
       way[:source] = widget_id
       
       link_to_event(title, way, html_options)
     end
     
     
-    # the standard way to get a form tag referencing a widget in the tree.
+    # Creates a form tag to the event controller and the targeted widget.
+    # The behaviour of the submit request is discussed in link_to_event.
+    # 
+    # Only provide +widget_id+ if you want another source widget for the resulting 
+    # Apotomo event than the current widget rendering the form.
+    ### DISCUSS: deprecate widget_id.
     def form_to_widget(way={}, widget_id=false, html_options={})
-    #def form_to_event()
-      way[:type] = :invoke
       addr = address_to_remote_widget(widget_id, way)
       
       form_remote_tag({:url => addr})
     end
     
-   end  
+    
+    # Creates a form tag to the event controller and the targeted widget.
+    # The behaviour of the submit request is discussed in link_to_event.
+    # 
+    def form_to_event(way={}, html_options={})
+      addr = address_to_remote_widget(way[:source], way)
+      
+      form_remote_tag({:url => addr})
+    end
+    
+   end
   
   
 
---
+++ apotomo/test/unit/test_view_helper.rb
@@ -0,0 +1,136 @@
+### TODO: implement!
+require File.expand_path(File.dirname(__FILE__) + "/../test_helper")
+
+
+class ApotomoViewHelperTestCell < Apotomo::Widget
+  helper Apotomo::ViewHelper
+  
+  def self.view_for_state(action_view, state)
+    File.expand_path(File.dirname(__FILE__)) + "/../views/#{state}.haml"
+  end
+  
+  
+  def state_with_address_to_remote_widget
+  end
+end
+
+class ViewHelperModelTree < Apotomo::WidgetTree
+  def draw_model_tree(root)
+    domain('param_domain', 'booze_id' => 1) << root
+      #root << cell(:booze, :info, 'booze_info')
+      #root << cell(:booze, :edit, 'booze_edit', :states => [:update])
+      root << widget_factory.widget("apotomo_view_helper_test_cell", 'test_cell', :state_with_address_to_remote_widget)
+  end
+end
+
+class ApplicationModelTree < ViewHelperModelTree; end;
+
+
+
+class ViewHelperTest < Test::Unit::TestCase
+  include Apotomo::UnitTestCase
+  
+  include Apotomo::ViewHelper
+  include ActionView::Helpers::PrototypeHelper
+  include ActionView::Helpers::JavaScriptHelper
+  include ActionView::Helpers::UrlHelper
+  include ApplicationHelper
+  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::FormTagHelper
+  
+  
+  # extend StatefulWidget for testing purposes.
+  Apotomo::StatefulWidget.class_eval do
+    cattr_accessor :current_widget
+  end
+  
+  
+  def setup
+    super
+    
+    @a = Apotomo::StatefulWidget.new(@controller, 'a')
+    @b = Apotomo::StatefulWidget.new(@controller, 'b')
+    @a << @b
+  end
+  
+  # test Apotomo::ViewHelper methods --------------------------
+  
+  def test_current_tree
+    Apotomo::StatefulWidget.current_widget = @a
+    assert_equal current_tree, @a # --> #current_tree should return the root.
+  end
+  
+  
+  def test_target_widget_for
+    Apotomo::StatefulWidget.current_widget = @a
+    assert_equal target_widget_for(), @a
+    assert_equal target_widget_for('b'), @b
+  end
+  
+  
+  def test_static_link_to_widget
+    get :index
+    Apotomo::StatefulWidget.current_widget = @a
+    l = static_link_to_widget("Static Link", false, :static => true)
+    puts l
+    assert_no_match /static=/, l
+  end
+  
+  def test_link_to_widget
+    get :index
+    Apotomo::StatefulWidget.current_widget = @b
+    l = link_to_widget("Static Link")
+    puts l
+    assert_no_match /static=/, l
+    assert_match /source=b/, l
+  end
+  
+  def test_link_to_event
+    get :index
+    Apotomo::StatefulWidget.current_widget = @a
+    
+    # test explicit source ------------------------------------
+    l = link_to_event("Event Link", :source => 'b')
+    assert_match /source=b/, l
+    assert_no_match /source=a/, l
+    
+    # test default source widget ------------------------------
+    l = link_to_event("Event Link")
+    assert_match /source=a/, l
+    assert_no_match /source=b/, l
+    # test implicit type --------------------------------------
+    assert_no_match /type=/, l
+    
+    # test explicit type --------------------------------------
+    l = link_to_event("Event Link", :type => :click)
+    assert_match /type=click/, l
+  end
+  
+  def dont_test_form_to_event
+    get :index
+    Apotomo::StatefulWidget.current_widget = @b
+    
+    # test default source -------------------------------------
+    l = form_to_widget
+    assert_match /source=b/, l
+  end
+
+
+  def test_address_to_remote_widget
+    Apotomo::StatefulWidget.current_widget = @a
+    
+    assert_equal Apotomo::StatefulWidget.current_widget, @a
+    
+    addr = address_to_remote_widget()
+    #puts addr.inspect
+    assert addr[:source], 'a'
+    
+    addr = address_to_remote_widget('b')
+    assert addr[:source], 'b'
+    
+    addr = address_to_remote_widget(false, :param_1 => 'one')
+    assert addr[:source],  'a'
+    assert addr[:param_1],    'one'
+  end
+  
+end
