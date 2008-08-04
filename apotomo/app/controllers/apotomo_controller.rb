4f1bf79bb43b1d5332ae4617158ad9f3f4ab4b0a
Nick Sutterer <nick@downfall.(none)>
2008-08-04 22:12:44 +0200

    Added #iframe2event in ApotomoController, so we can receive and process events from iframes.

--- apotomo/app/controllers/apotomo_controller.rb
+++ apotomo/app/controllers/apotomo_controller.rb
@@ -1,5 +1,9 @@
 class ApotomoController < ApplicationController
   
+  ### NOTE: i KNOW i could extract lots of lines out of all those actions, but as long 
+  ###  as we haven't a complete proof-of-concept we will have duplicate code :-P#
+  
+  
   # The central action for events within the Apotomo environment.
   # Any event is sent to this action. It just triggers the event,
   # the processing happens through the widget tree. 
@@ -35,7 +39,43 @@ class ApotomoController < ApplicationController
   
   
   ### FIXME: duplicate code sucks!
+  def iframe2event
+    tree      = ApplicationWidgetTree.new(self).draw_tree.root
+    processor = Apotomo::EventProcessor.instance
+    processor.init
+    
+    type = params[:type] || :invoke
+    
+    
+    tree.find_by_id(params[:source]).trigger(type.to_sym)
+    
+    processed_handlers = processor.process_queue_for(tree, nil)
+    
+    script = ""
+    
+    processed_handlers.each do |handler|
+        ### DISCUSS: i don't like that switch, but moving this behaviour into the
+        ###   actual handler is too complicated, as we just need replace and exec.
+        content = handler.content
+        next unless content ### DISCUSS: move this decision into processed_handlers#each.
+        
+        if content.class == String
+          script += 'Element.replace("'+handler.widget_id+'", "'+content.gsub('"', '\\\\\"').gsub("\n", "").gsub("'", "\\\\'")+'");'
+          #page.replace handler.widget_id, content
+        else
+          raise ""
+          page << content
+        end
+      end
+    
+    render :text => "<html><body><script type='text/javascript' charset='utf-8'>
+        var loc = document.location;
+        with(window.parent) { setTimeout(function() { window.eval('#{script}'); loc.replace('about:blank'); }, 1) } 
+      </script></body></html>"
+  end
   
+  
+  ### FIXME: duplicate code sucks!
   def data
     tree = ApplicationWidgetTree.new(self).draw_tree.root
     
