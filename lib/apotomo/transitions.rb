b60b87d7f2e0e172496f56620d66c05b015681cc
Nick Sutterer <nick@riot.(none)>
2009-03-28 18:35:54 +0100

    added StatefulWidget#transition to declaratively define allowed transitions in the widget class.

--- lib/apotomo/transitions.rb
+++ lib/apotomo/transitions.rb
@@ -1,11 +1,61 @@
-### DISCUSS: it's a module so we can better test it. is this good?
 module Apotomo::Transitions
   
-  def transitions; {}; end
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+  
+  module ClassMethods
+    ### TODO: document me, as soon as DSL is considered stable.
+    def transition(options)
+      if from = options[:from]
+        class_transitions[from] ||= []
+        class_transitions[from] << options[:to]
+        return
+      end
+      
+      if loop = options[:in]
+        transition :from => loop, :to => loop
+      end
+    end
+    
+    def class_transitions; @class_transitions ||= {}; end
+  end
+  
+  # Returns list of allowed transitions, which can be set in #transition_map and
+  # with successive calls to #transition in the widget class.
+  # Note that #transition_map will take precedence.
+  def transitions; 
+    self.class.class_transitions.merge(transition_map)
+  end
+  
+  # Explicitly defines the valid state transistions for this widget instance.
+  #
+  # Example:
+  #   def transition_map
+  #     { :start_state_one  => [:some_state, :looping_state],
+  #       :looping_state    => [:looping_state]
+  #     }
+  #   end
+  #
+  # This would create a state machine like
+  #
+  #
+  #                        |---> :some_state
+  #   :start_state_one --->|
+  #                        |---> :looping_state ---
+  #                                         ^     |
+  #                                         |     |
+  #                                         -------
+  # Should be overridden by widgets, if they need to define a transition map per object.
+  # Successive calls to #transition in the widget class should be sufficient for most needs.
+  def transition_map; {};             end
+  
+  # Returns list of initial states which are set in widget constructor.
+  def start_states; @start_states;    end
   
   
   def start_state?(state)
-    @start_states.include?(state)
+    start_states.include?(state)
   end
   
   def valid_next_state?(last_state, state)
@@ -30,7 +80,7 @@ module Apotomo::Transitions
   
   
   def default_start_state
-    @start_states.first
+    start_states.first
   end
   
   def default_next_state_for(last_state)
