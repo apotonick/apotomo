11c8316e8899126be37ee361f05cf802fdb6933e
Nick Sutterer <apotonick@gmail.com>
2010-03-15 14:27:06 +0100

    Event is now properly derived from Onfire::Event. added tests.

--- lib/apotomo/event.rb
+++ lib/apotomo/event.rb
@@ -1,15 +1,7 @@
 module Apotomo
   # Events are created by Apotomo in #fire. They bubble up from their source to root and trigger
   # event handlers. 
-  class Event
-    attr_accessor :type, :source, :data
-    
-    def initialize(type, source=nil, data={})
-      @type       = type
-      @source     = source
-      @data       = data
-    end
-    
+  class Event < Onfire::Event
     def _dump(depth)
       raise "You're trying to serialize an instance of Apotomo::Event. Don't do that."
     end
---
+++ test/unit/event_test.rb
@@ -0,0 +1,30 @@
+require File.join(File.dirname(__FILE__), *%w[.. test_helper])
+ 
+class EventTest < Test::Unit::TestCase
+  context "An Event" do
+    should "require type and respond to #type" do
+      assert_equal :footsteps, Apotomo::Event.new(:footsteps).type
+    end
+    
+    should "require source and respond to #source" do
+      @event = Apotomo::Event.new(:footsteps, 'mum')
+      assert_equal :footsteps,  @event.type
+      assert_equal 'mum',       @event.source
+    end
+    
+    should "accept an additional data object and respond to #data" do
+      @event = Apotomo::Event.new(:footsteps, 'mum', {:volume => :loud})
+      assert_equal({:volume => :loud}, @event.data)
+    end
+    
+    should "complain when serialized" do
+      assert_raises RuntimeError do
+        Marshal.dump(Apotomo::Event.new(:footsteps, 'mum'))
+      end
+    end
+    
+    should "respond to #stopped?" do
+      assert_not Apotomo::Event.new(:footsteps, 'mum').stopped?
+    end
+  end
+end
