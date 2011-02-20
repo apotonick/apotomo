5a398d8cd2f7a921a63d178f53d2bca745febc39
Nick Sutterer <apotonick@gmail.com>
2011-02-20 18:21:38 +0100

    railtie into separate file.

--- lib/apotomo.rb
+++ lib/apotomo.rb
@@ -1,32 +1,8 @@
-#  Copyright (c) 2007-2010 Nick Sutterer <apotonick@gmail.com>
-#  
-#  The MIT License
-#  
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-#  
-#  The above copyright notice and this permission notice shall be included in
-#  all copies or substantial portions of the Software.
-#  
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-#  THE SOFTWARE.
-
-require "rails/railtie"
- 
 module Apotomo
   class << self
     def js_framework=(js_framework)
       @js_framework = js_framework
-      @js_generator = ::Apotomo::JavascriptGenerator.new(js_framework)
+      @js_generator = JavascriptGenerator.new(js_framework)
     end
     
     attr_reader :js_generator, :js_framework
@@ -42,35 +18,13 @@ module Apotomo
       yield self
     end
   end
-  
-  # Piotr Sarnacki: Railtie :P
-  class Railtie < Rails::Railtie
-    rake_tasks do
-      load "apotomo/apotomo.rake"
-    end
-    
-    # As we are a Railtie only, the routes won't be loaded automatically. Beside that, we want our 
-    # route to be the very first (otherwise #resources might supersede it).
-    initializer :prepend_apotomo_routes, :after => :add_routing_paths do |app|
-      app.routes_reloader.paths.unshift(File.dirname(__FILE__) + "/../config/routes.rb")
-    end
-    
-    # Include a lazy loader via has_widgets.
-    initializer :add_has_widgets do |app|
-      ActionController::Base.extend Apotomo::Rails::ControllerMethodsLoader
-    end
-  end 
 end
 
-
 require 'apotomo/widget'
-require 'apotomo/container_widget'
+require 'apotomo/railtie'
 require 'apotomo/widget_shortcuts'
 require 'apotomo/rails/controller_methods'
-
-
 require 'apotomo/javascript_generator'
-Apotomo.js_framework = :jquery ### DISCUSS: move to rails.rb
+require 'apotomo/test_case' if Rails.env == "test"
 
-### FIXME: only load in test env.
-require 'apotomo/test_case' #if defined?("Rails") and Rails.env == "test"
+Apotomo.js_framework = :jquery ### DISCUSS: move to rails.rb
