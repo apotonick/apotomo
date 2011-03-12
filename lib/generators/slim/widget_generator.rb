c715ce03dfcae590ef8bb800638610c8eb166d42
bluntpeak projects <huw.nichols@gmail.com>
2011-03-12 23:17:45 +0000

    Add slim template support to widget generator via -e slim option

---
+++ lib/generators/slim/widget_generator.rb
@@ -0,0 +1,11 @@
+require 'generators/slim/cell_generator'
+require 'generators/apotomo/widget_generator'
+
+module Slim
+  module Generators
+    class WidgetGenerator < CellGenerator
+      include ::Apotomo::Generators::BasePathMethods
+      source_root File.expand_path('../../templates', __FILE__)
+    end
+  end
+end
