2b11eeb86b0d34c9150ff1e2c7569b01d0f7e95e
Bernie Telles <btelles@gmail.com>
2011-03-23 21:25:30 -0400

    Add several helpers to the jquery version of the javascript helpers.

--- lib/apotomo/javascript_generator.rb
+++ lib/apotomo/javascript_generator.rb
@@ -4,11 +4,11 @@ module Apotomo
       raise "No JS framework specified" if framework.blank?
       extend "apotomo/javascript_generator/#{framework}".camelize.constantize
     end
-    
+
     def <<(javascript)
       "#{javascript}"
     end
-    
+
     # Copied from ActionView::Helpers::JavascriptHelper.
     JS_ESCAPE_MAP = {
       '\\'    => '\\\\',
@@ -22,14 +22,14 @@ module Apotomo
     # Escape carrier returns and single and double quotes for JavaScript segments.
     def self.escape(javascript)
       return javascript.gsub(/(\\|<\/|\r\n|[\n\r"'])/) { JS_ESCAPE_MAP[$1] } if javascript
-      
+
       ''
     end
-    
+
     def escape(javascript)
       self.class.escape(javascript)
     end
-    
+
     module Prototype
       def prototype;              end
       def element(id);            "$(\"#{id}\")"; end
@@ -38,7 +38,7 @@ module Apotomo
       def update_id(id, markup);  update(id, markup); end
       def replace_id(id, markup); replace(id, markup); end
     end
-    
+
     module Right
       def right;                  end
       def element(id);            "$(\"#{id}\")"; end
@@ -47,7 +47,7 @@ module Apotomo
       def update_id(id, markup);  update(id, markup); end
       def replace_id(id, markup); replace(id, markup); end
     end
-    
+
     module Jquery
       def jquery;                 end
       def element(id);            "$(\"#{id}\")"; end
@@ -55,6 +55,18 @@ module Apotomo
       def replace(id, markup);    element(id) + '.replaceWith("'+escape(markup)+'");'; end
       def update_id(id, markup);  update("##{id}", markup); end
       def replace_id(id, markup); replace("##{id}", markup); end
+
+      %w{append prepend before after wrap}.each do |method_with_markup|
+        define_method(method_with_markup.to_sym) do |id, markup|
+          "#{element(id)}.#{method_with_markup}(\"#{escape(markup)}\");"
+        end
+      end
+
+      %w{detach empty clone remove}.each do |method_direct_params|
+        define_method(method_direct_params.to_sym) do |id, direct_params|
+          "#{element(id)}.#{method_direct_params}(#{direct_params});"
+        end
+      end
     end
   end
 end
