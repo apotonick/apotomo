63a28fe061ea2590c7de099c4d0e8811aa581ae6
Nick Sutterer <apotonick@gmail.com>
2011-09-27 11:11:34 +0200

    use default id in generated widget test.

--- lib/generators/templates/widget_test.rb
+++ lib/generators/templates/widget_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class <%= class_name %>WidgetTest < Apotomo::TestCase
   has_widgets do |root|
-    root << widget(:<%= file_name %>, 'me')
+    root << widget(:<%= file_name %>)
   end
   
   test "display" do
-    render_widget 'me'
+    render_widget :<%= file_name %>
     assert_select "h1"
   end
 end
