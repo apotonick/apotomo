2e8b36d628073bc9ad29669d47ed7402600d07db
Kristian Mandrup <kmandrup@gmail.com>
2012-09-05 15:59:05 +0200

    added api docs

--- lib/apotomo/javascript_generator.rb
+++ lib/apotomo/javascript_generator.rb
@@ -24,27 +24,34 @@ module Apotomo
     
     module Prototype
       def prototype;              end
-      def element(id);            "$(\"#{id}\")"; end
-      def update(id, markup);     element(id) + '.update("'+escape(markup)+'");'; end
-      def replace(id, markup);    element(id) + '.replace("'+escape(markup)+'");'; end
-      def update_id(id, markup);  update(id, markup); end
-      def replace_id(id, markup); replace(id, markup); end
+      def element(selector);            "$(\"#{selector}\")"; end
+      def update(selector, markup);     element(selector) + '.update("'+escape(markup)+'");'; end
+      def replace(selector, markup);    element(selector) + '.replace("'+escape(markup)+'");'; end
+
+      alias_method :update_id, :update
+      alias_method :replace_id, :replace
     end
     
     module Right
       def right;                  end
-      def element(id);            "$(\"#{id}\")"; end
-      def update(id, markup);     element(id) + '.update("'+escape(markup)+'");'; end
-      def replace(id, markup);    element(id) + '.replace("'+escape(markup)+'");'; end
-      def update_id(id, markup);  update(id, markup); end
-      def replace_id(id, markup); replace(id, markup); end
+      def element(selector);            "$(\"#{selector}\")"; end
+      def update(selector, markup);     element(selector) + '.update("'+escape(markup)+'");'; end
+      def replace(selector, markup);    element(selector) + '.replace("'+escape(markup)+'");'; end
+
+      alias_method :update_id, :update
+      alias_method :replace_id, :replace
     end
     
     module Jquery
       def jquery;                 end
-      def element(id);            "$(\"#{id}\")"; end
+      def element(selector)
+        selector = selector =~ /^_apo_/ ? selector : "'#{selector}'"
+        "$(#{selector})"
+      end
+
       def update(id, markup);     element(id) + '.html("'+escape(markup)+'");'; end
       def replace(id, markup);    element(id) + '.replaceWith("'+escape(markup)+'");'; end
+
       def update_id(id, markup);  update("##{id}", markup); end
       def replace_id(id, markup); replace("##{id}", markup); end
 
@@ -52,94 +59,140 @@ module Apotomo
         element(id) + ".find(#{selector}).text('#{escape(markup)}');"
       end
 
+      def selector_for var, id, selector
+        raise ArgumentError, "Must not be an _apo_ selector here: #{selector}" if apo_match?(selector)
+        "var _apo_#{var} = " + element(id) + ".find('#{selector}');"
+      end
+
+      def find_element id, selector
+        element(id) + ".find('#{selector}')"
+      end
+
       def append(id, selector, markup)
-        element(id) + ".find(#{selector}).append('#{escape(markup)}');"
+        find_element(id, selector) + ".append(#{escaped(markup)});"
       end
 
       def prepend(id, selector, markup)
-        element(id) + ".find(#{selector}).prepend('#{escape(markup)}');"
+        find_element(id, selector) + ".prepend(#{escaped(markup)});"
       end
 
       def append_to(selector, markup)
-        "$(#{escape(markup)}').appendTo('#{selector}');"
+        selector = calc_selector selector
+        "$(#{escaped(markup)}).appendTo(#{selector});"
       end
 
       def prepend_to(selector, markup)
-        "$(#{escape(markup)}').prependTo('#{selector}');"
+        selector = calc_selector selector
+        "$(#{escaped(markup)}).prependTo(#{selector});"
       end
 
       def after selector, markup
-        "$('#{selector}').after('#{escape(markup)}');"
+        selector = calc_selector selector
+        element(selector) + ".after(#{escaped(markup)});"
       end
 
       def before selector, markup
-        "$('#{selector}').before('#{escape(markup)}');"
+        selector = calc_selector selector
+        element(selector) + ".before(#{escaped(markup)});"
       end
 
       def replace_all(selector, markup)
-        "$(#{escape(markup)}').replaceAll('#{selector}');"
+        selector = calc_selector selector
+        "$(#{escaped(markup)}).replaceAll(#{selector});"
       end    
 
-      def unwrap (selector)
-        "$('#{selector}').unwrap();"
+      def unwrap(selector)
+        selector = calc_selector selector
+        element(selector) + ".unwrap();"
       end    
 
-      def wrap (selector, markup)
-        "$('#{selector}').wrap('#{escape(markup)}');"
+      def wrap(selector, markup)
+        selector = calc_selector selector
+        element(selector) + ".wrap(#{escaped(markup)});"
       end    
 
-      def wrap_innet (selector, markup)
-        "$('#{selector}').wrapInner('#{escape(markup)}');"
+      def wrap_inner(selector, markup)
+        selector = calc_selector selector
+        element(selector) + ".wrapInner(#{escaped(markup)});"
       end    
 
-      def wrap_all (selector, markup)
-        "$('#{selector}').wrap_all('#{escape(markup)}');"
+      def wrap_all(selector, markup)
+        selector = calc_selector selector
+        element(selector) + ".wrap_all(#{escaped(markup)});"
       end    
 
       def remove(selector)
-        "$('#{selector}').remove();"
+        selector = calc_selector selector
+        element(selector) + ".remove();"
       end
 
       def remove_class(selector, *classes)
         classes = classes.flatten.join(' ')
-        "$('#{selector}').removeClass('#{classes}');"
+        selector = calc_selector selector
+        element(selector) + ".removeClass('#{classes}');"
       end
       alias_method :remove_classes, :remove_class
 
       def add_class(selector, *classes)
         classes = classes.flatten.join(' ')
-        "$('#{selector}').addClass('#{classes}');"
+        selector = calc_selector selector
+        element(selector) + ".addClass('#{classes}');"
       end
       alias_method :add_classes, :add_class
 
       def toggle_class(selector, *classes)
         classes = classes.flatten.join(' ')
-        "$('#{selector}').toggleClass('#{classes}');"
+        selector = calc_selector selector
+        element(selector) + ".toggleClass('#{classes}');"
       end
       alias_method :toggle_classes, :toggle_class
 
-      def toggle_class_fun(selector, fun)        
-        "$('#{selector}').toggleClass(function() {#{fun}});"
+      def toggle_class_fun(selector, fun)
+        selector = calc_selector selector
+        element(selector) + ".toggleClass(function() {#{fun}});"
       end
 
       def get_attr(selector, name)
-        "$('#{selector}').attr('#{name}');"
+        selector = calc_selector selector
+        element(selector) + ".attr('#{name}');"
       end
 
       def get_prop(selector, name)
-        "$('#{selector}').prop('#{name}');"
+        selector = calc_selector selector
+        element(selector) + ".prop('#{name}');"
       end
 
       def get_val selector
-        "$('#{selector}').val();"
+        selector = calc_selector selector
+        element(selector) + ".val();"
       end
 
       def get_html(selector)
-        "$('#{selector}').html();"
+        selector = calc_selector selector
+        element(selector) + ".html();"
       end
 
       def empty(selector)
-        "$('#{selector}').empty();"
+        selector = calc_selector selector
+        element(selector) + ".empty();"
+      end
+
+      private 
+
+      def escaped markup
+        "'#{escape(markup)}'"
+      end
+
+      def apo_match? selector
+        selector.to_s =~ /^_apo_/
+      end
+
+      def calc_selector selector
+        selector = apo_selector?(selector) ? "_apo_#{selector}" : "'#{selector}'"
+      end
+
+      def apo_selector? selector
+        selector.kind_of?(Symbol) && selector.to_s[0..1] == '_'
       end
     end
   end
