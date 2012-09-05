492dc03de1c480f5b2d6172378e48ecdcb5d9ade
Kristian Mandrup <kmandrup@gmail.com>
2012-09-05 16:57:52 +0200

    much cleaner and flexible

--- lib/apotomo/javascript_generator.rb
+++ lib/apotomo/javascript_generator.rb
@@ -61,137 +61,152 @@ module Apotomo
 
       def selector_for var, id, selector
         raise ArgumentError, "Must not be an _apo_ selector here: #{selector}" if apo_match?(selector)
-        "var _apo_#{var} = " + element(id) + ".find('#{selector}');"
+        "var _apo_#{var} = " + element("##{id}") + ".find('#{selector}');"
       end
 
-      def find_element id, selector
-        element(id) + ".find('#{selector}')"
+      def find_element id, selector        
+        if id == nil || apo_selector?(selector)
+          return element(calc_selector selector)
+        end
+        element("##{id}") + ".find('#{selector}')"
       end
 
-      def append(id, selector, markup)
-        find_element(id, selector) + ".append(#{escaped(markup)});"
+      [:replace_all, :prepend_to, :append_to].each do |name|
+        define_method name do |selector, markup|
+          _jq_inverse_action(selector, markup, name.to_sym)
+        end
       end
 
-      def prepend(id, selector, markup)
-        find_element(id, selector) + ".prepend(#{escaped(markup)});"
+      [:append, :prepend, :after, :before, :wrap, :wrap_inner, :wrap_all].each do |name|
+        define_method name do |args|      
+          _jquery_action *args, name
+        end
       end
 
-      def append_to(selector, markup)
-        selector = calc_selector selector
-        "$(#{escaped(markup)}).appendTo(#{selector});"
-      end
-
-      def prepend_to(selector, markup)
-        selector = calc_selector selector
-        "$(#{escaped(markup)}).prependTo(#{selector});"
-      end
-
-      def after selector, markup
-        selector = calc_selector selector
-        element(selector) + ".after(#{escaped(markup)});"
-      end
-
-      def before selector, markup
-        selector = calc_selector selector
-        element(selector) + ".before(#{escaped(markup)});"
-      end
-
-      def replace_all(selector, markup)
-        selector = calc_selector selector
-        "$(#{escaped(markup)}).replaceAll(#{selector});"
+      def unwrap *args
+        jquery_action *args, 'unwrap()'
       end    
 
-      def unwrap(selector)
-        selector = calc_selector selector
-        element(selector) + ".unwrap();"
-      end    
-
-      def wrap(selector, markup)
-        selector = calc_selector selector
-        element(selector) + ".wrap(#{escaped(markup)});"
-      end    
-
-      def wrap_inner(selector, markup)
-        selector = calc_selector selector
-        element(selector) + ".wrapInner(#{escaped(markup)});"
-      end    
-
-      def wrap_all(selector, markup)
-        selector = calc_selector selector
-        element(selector) + ".wrap_all(#{escaped(markup)});"
-      end    
-
-      def remove(selector)
-        selector = calc_selector selector
-        element(selector) + ".remove();"
+      def remove *args
+        _jquery_action *args, 'remove()'
       end
 
-      def remove_class(selector, *classes)
+      def remove_class(id, selector, *classes)
         classes = classes.flatten.join(' ')
-        selector = calc_selector selector
-        element(selector) + ".removeClass('#{classes}');"
+        find_element(id, selector) + _js_action("removeClass('#{classes}')")
       end
       alias_method :remove_classes, :remove_class
 
-      def add_class(selector, *classes)
+      def add_class(id, selector, *classes)
         classes = classes.flatten.join(' ')
-        selector = calc_selector selector
-        element(selector) + ".addClass('#{classes}');"
+        find_element(id, selector) + _js_action("addClass('#{classes}')"
       end
       alias_method :add_classes, :add_class
 
-      def toggle_class(selector, *classes)
+      def toggle_class(id, selector, *classes)
         classes = classes.flatten.join(' ')
-        selector = calc_selector selector
-        element(selector) + ".toggleClass('#{classes}');"
+        find_element(id, selector) + _js_action("toggleClass('#{classes}')")
       end
       alias_method :toggle_classes, :toggle_class
 
-      def toggle_class_fun(selector, fun)
-        selector = calc_selector selector
-        element(selector) + ".toggleClass(function() {#{fun}});"
+      def toggle_class_fun(id, selector, fun)        
+        find_element(id, selector) + _js_action("toggleClass(function() {#{fun}})")
       end
 
-      def get_attr(selector, name)
-        selector = calc_selector selector
-        element(selector) + ".attr('#{name}');"
+      def get_attr(id. selector, name)
+        find_element(id, selector) + _js_action("attr('#{name}')")
       end
 
-      def get_prop(selector, name)
-        selector = calc_selector selector
-        element(selector) + ".prop('#{name}');"
+      def get_prop(selector, name)        
+        find_element(id, selector) + _js_action("prop('#{name}')")
       end
 
-      def get_val selector
-        selector = calc_selector selector
-        element(selector) + ".val();"
+      def get_val selector        
+        find_element(id, selector) + _js_action("val()")
       end
 
       def get_html(selector)
-        selector = calc_selector selector
-        element(selector) + ".html();"
+        find_element(id, selector) + _js_action("html()")
       end
 
       def empty(selector)
-        selector = calc_selector selector
-        element(selector) + ".empty();"
+        find_element(id, selector) + _js_action("empty()")
       end
 
       private 
 
+      def _jq_inverse_action(selector, markup, action)
+        selector = calc_selector selector
+        action = _js_camelize(action)
+        "$(#{escaped(markup)}).#{action}(#{selector});"
+      end      
+
+      def _jquery_action *args, &block
+        args = args.flatten    
+        [id, selector, markup, action] = _extract_args(*args)
+        action ||= yield if block_given?
+        raise ArgumentError, "Must take action block or Symbol as last argument" unless action
+        elem_action = case action
+        when String
+          action = _js_camelize(action)
+          ".#{action};"
+        else
+          action = _js_camelize(action)
+          _action(action, markup)
+        end
+        find_element(id, selector) + elem_action
+      end
+                
       def escaped markup
         "'#{escape(markup)}'"
+      end      
+
+      def _action name, markup        
+        _js_action "#{name}(#{_escaped(markup)})"
       end
 
-      def apo_match? selector
+      def _js_action action
+        ".#{action};"
+      end
+
+      def _js_camelize str
+        str.to_s.camelize.sub(/^\w/, s[0].downcase)
+      end
+
+      # id, selector, markup, action
+      def _extract_args *args
+        [_extract_id(*args), _extract_selector(*args), _extract_markup(*args), _extract_action(*args)]
+      end
+
+      def _extract_id *args
+        args = args.flatten
+        args.size == 4 ? args.first : nil
+      end
+
+      def _extract_selector *args
+        args = args.flatten
+        args.size == 4 ? args[1] : args[0]
+      end
+
+      def _extract_markup(*args)
+        args = args.flatten
+        args.size == 4 ? args[2] : args.last
+      end
+
+      def _extract_action(*args)
+        args = args.flatten
+        args.size == 4 ? args.last : nil
+      end
+
+      def _apo_match? selector
         selector.to_s =~ /^_apo_/
       end
 
-      def calc_selector selector
-        selector = apo_selector?(selector) ? "_apo_#{selector}" : "'#{selector}'"
+      def _calc_selector selector
+        selector = _apo_selector?(selector) ? "_apo_#{selector}" : "'#{selector}'"
       end
 
-      def apo_selector? selector
+      def _apo_selector? selector
         selector.kind_of?(Symbol) && selector.to_s[0..1] == '_'
       end
     end
