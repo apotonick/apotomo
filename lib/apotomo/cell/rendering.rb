a6d026edf8bdef0d882a0ab696e33dec3a08ab95
Kristian Mandrup <kmandrup@gmail.com>
2012-11-22 09:25:05 +0100

    fix rendering?

--- lib/apotomo/cell/rendering.rb
+++ lib/apotomo/cell/rendering.rb
@@ -13,10 +13,14 @@ module Cell
       end
       
       view_name ||= File.join('views', self.action_name || '')
+      # puts "view_name: #{view_name}"
+
       render_view_for(view_name, *args)
     rescue
       # try without view
       view_name = view_name.gsub(/views\//, '')
+      # puts "TRY view_name: #{view_name}"
+
       if args.first.kind_of?(Hash) && args.first[:view]
         hash = args.first
         hash[:view] = File.join(hash[:view].to_s)
