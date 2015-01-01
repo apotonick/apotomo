4ee826b0a7af0255bfcc5915bada074863d07ec2
Oleg Yashchuk <oazoer@gmail.com>
2015-01-01 06:32:38 +0300

    Fix #152

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  match ":controller/render_event_response", :to => "#render_event_response", :as => "apotomo_event", :via =>  [:get, :post, :put, :patch, :delete]
+  match ":controller/render_event_response", :action => "render_event_response", :as => "apotomo_event", :via =>  [:get, :post, :put, :patch, :delete]
 end
