3e2d5068856933882fd2da655bf9aceb363d3352
Ben Li <ben@kwelasolutions.com>
2020-02-18 15:47:15 +1100

    do not load apotomo routes in Rails 5.1 and above as dynamic :controller segment is now deprecated

--- lib/apotomo/railtie.rb
+++ lib/apotomo/railtie.rb
@@ -6,10 +6,14 @@ module Apotomo
       load "apotomo/apotomo.rake"
     end
 
-    # As we are a Railtie only, the routes won't be loaded automatically. Beside that, we want our
-    # route to be the very first (otherwise #resources might supersede it).
-    initializer 'apotomo.prepend_routes', :after => :add_routing_paths do |app|
-      app.routes_reloader.paths.unshift(File.dirname(__FILE__) + "/../../config/routes.rb")
+    # In Rails 5.1, a dynamic :controller segment in a route is deprecated.
+    # The routes for each controller have to be added in routes.rb.
+    if ::ActiveRecord.gem_version < ::Gem::Version.new("5.1.0.beta1")
+      # As we are a Railtie only, the routes won't be loaded automatically. Beside that, we want our
+      # route to be the very first (otherwise #resources might supersede it).
+      initializer 'apotomo.prepend_routes', :after => :add_routing_paths do |app|
+        app.routes_reloader.paths.unshift(File.dirname(__FILE__) + "/../../config/routes.rb")
+      end
     end
 
     # Include a lazy loader via has_widgets.
